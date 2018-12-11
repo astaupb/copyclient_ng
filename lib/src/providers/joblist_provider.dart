1b8b9af74efd583ab7f0ee70ac5a3e03cbe0285b
Leon Tappe <ltappe@mail.uni-paderborn.de>
2018-12-11 03:31:23 +0100

    add provider for joblistbloc

---
+++ lib/src/providers/joblist_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:html';
+
+import 'package:angular/core.dart';
+import 'package:blocs_copyclient/joblist.dart';
+import 'package:http/browser_client.dart';
+
+import '../backend_sunrise.dart';
+
+@Injectable()
+class JoblistProvider {
+  static String _token;
+  static final JoblistProvider _singleton = JoblistProvider._internal(
+    JoblistBloc(BackendSunrise(BrowserClient()), _token),
+  );
+
+  JoblistBloc joblistBloc;
+
+  factory JoblistProvider() => _singleton;
+
+  JoblistProvider._internal(this.joblistBloc) {
+    _token =
+        window.localStorage['token'] ?? window.sessionStorage['token'] ?? '';
+  }
+}
