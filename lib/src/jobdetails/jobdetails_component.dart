9f4588def90447f55e736a7c7ed2eae51b8c7f94
Michael Koch <mkoch@asta.upb.de>
2019-03-01 14:20:53 +0100

    add material dropdwon selector for duplex

--- lib/src/jobdetails/jobdetails_component.dart
+++ lib/src/jobdetails/jobdetails_component.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:angular/angular.dart';
 import 'package:angular_bloc/angular_bloc.dart';
 import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_list/material_list_item.dart';
+import 'package:angular_components/material_select/material_dropdown_select.dart';
 import 'package:angular_components/material_toggle/material_toggle.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:blocs_copyclient/joblist.dart';
@@ -27,6 +29,8 @@ import '../route_paths.dart';
     NgIf,
     MaterialButtonComponent,
     MaterialToggleComponent,
+    MaterialListItemComponent,
+    MaterialDropdownSelectComponent,
   ],
   pipes: [
     BlocPipe,
@@ -39,6 +43,20 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
   PreviewBloc previewBloc;
   Location _location;
   Job job;
+
+  bool color;
+  int duplex;
+  int copies;
+  bool collate;
+  bool a3;
+  String range;
+  int nup;
+  int nupPageOrder;
+  bool keep;
+
+  final List<String> duplexOptions = ['simplex', 'kurze Kante', 'lange Kante'];
+  String duplexSelection = 'simplex';
+
   double estimatedDouble = 0.0;
   List<List<int>> previews;
 
@@ -55,6 +73,18 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
 
   void goBack() => _location.back();
 
+  void setJobOptions(JobOptions options) {
+    this.color = options.color;
+    this.duplex = options.duplex;
+    this.copies = options.copies;
+    this.collate = options.collate;
+    this.a3 = options.a3;
+    this.range = options.range;
+    this.nup = options.nup;
+    this.nupPageOrder = options.nupPageOrder;
+    this.keep = options.keep;
+  }
+
   @override
   void onActivate(_, RouterState current) async {
     int id = getId(current.parameters);
@@ -62,6 +92,7 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
       joblistBloc.state.listen((state) {
         if (state.isResult) {
           job = state.value.singleWhere((Job job) => job.id == id);
+          setJobOptions(job.jobOptions);
           estimatedDouble = (job.priceEstimation as double) / 100.0;
           previewBloc.getPreview(job);
         }
@@ -73,9 +104,8 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
               .previews;
         }
       });
-      if (joblistBloc.jobs.isEmpty) {
+      if (joblistBloc.jobs != null && joblistBloc.jobs.isEmpty)
         joblistBloc.onRefresh();
-      }
     }
   }
 }
