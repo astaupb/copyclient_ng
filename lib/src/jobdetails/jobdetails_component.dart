2a2b93c1ce8d9e312263274a729139a3a5f9210a
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-03-01 23:49:54 +0100

    updated all joboption switches, now looking fine

--- lib/src/jobdetails/jobdetails_component.dart
+++ lib/src/jobdetails/jobdetails_component.dart
@@ -4,9 +4,12 @@ import 'dart:convert';
 import 'package:angular/angular.dart';
 import 'package:angular_bloc/angular_bloc.dart';
 import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_input/material_input.dart';
+import 'package:angular_components/material_list/material_list.dart';
 import 'package:angular_components/material_list/material_list_item.dart';
 import 'package:angular_components/material_select/material_dropdown_select.dart';
 import 'package:angular_components/material_toggle/material_toggle.dart';
+import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:blocs_copyclient/joblist.dart';
 import 'package:blocs_copyclient/preview.dart';
@@ -30,7 +33,11 @@ import '../route_paths.dart';
     MaterialButtonComponent,
     MaterialToggleComponent,
     MaterialListItemComponent,
+    MaterialListComponent,
     MaterialDropdownSelectComponent,
+    MaterialInputComponent,
+    materialInputDirectives,
+    formDirectives,
   ],
   pipes: [
     BlocPipe,
@@ -44,18 +51,29 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
   Location _location;
   Job job;
 
-  bool color;
-  int duplex;
-  int copies;
-  bool collate;
-  bool a3;
-  String range;
-  int nup;
-  int nupPageOrder;
-  bool keep;
+  bool color = true;
+  int duplex = 0;
+  int copies = 1;
+  bool collate = false;
+  bool a3 = false;
+  String range = '';
+  int nup = 1;
+  int nupPageOrder = 1;
+  bool keep = false;
 
-  final List<String> duplexOptions = ['simplex', 'kurze Kante', 'lange Kante'];
-  String duplexSelection = 'simplex';
+  final List<String> duplexOptions = ['Simplex', 'Kurze Kante', 'Lange Kante'];
+  String duplexSelection = 'Simplex';
+
+  final List<String> nupOptions = ['1', '2', '4'];
+  String nupSelection = '1';
+
+  final List<String> nupOrderOptions = [
+    'Nach Rechts, dann Runter',
+    'Nach Unten, dann Rechts',
+    'Nach Links, dann Runter',
+    'Nach Unten, dann Links',
+  ];
+  String nupOrderSelection = 'Nach Rechts, dann Runter';
 
   double estimatedDouble = 0.0;
   List<List<int>> previews;
@@ -97,6 +115,7 @@ class JobDetailsComponent extends AuthGuard implements OnActivate {
           previewBloc.getPreview(job);
         }
       });
+      
       previewBloc.state.listen((PreviewState state) {
         if (state.isResult) {
           previews = state.value
