f88cb1c095644b19a93e992dd5e4e04a9b090463
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-04-03 00:50:38 +0200

    move logout button and improve code

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -6,14 +6,10 @@ import 'package:angular/angular.dart';
 import 'package:angular_bloc/angular_bloc.dart';
 import 'package:angular_components/app_layout/material_persistent_drawer.dart';
 import 'package:angular_components/content/deferred_content.dart';
-import 'package:angular_components/laminate/enums/alignment.dart';
-import 'package:angular_components/laminate/popup/module.dart';
-import 'package:angular_components/laminate/popup/popup.dart';
 import 'package:angular_components/material_button/material_button.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
 import 'package:angular_components/material_list/material_list.dart';
 import 'package:angular_components/material_list/material_list_item.dart';
-import 'package:angular_components/material_popup/material_popup.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:blocs_copyclient/auth.dart';
 import 'package:blocs_copyclient/joblist.dart';
@@ -28,7 +24,6 @@ import 'package:http/src/client.dart';
 
 import 'src/auth_guard.dart';
 import 'src/backend_shiva.dart';
-import 'src/fullscreen_spinner.dart';
 import 'src/joblist/joblist_component.dart';
 import 'src/login/login_component.dart';
 import 'src/providers/auth_provider.dart';
@@ -41,11 +36,6 @@ import 'src/providers/pdf_provider.dart';
 import 'src/route_paths.dart';
 import 'src/routes.dart';
 
-@Injectable()
-PopupSizeProvider createPopupSizeProvider() {
-  return PercentagePopupSizeProvider();
-}
-
 @Component(
   selector: 'copyclient',
   styleUrls: [
@@ -55,7 +45,6 @@ PopupSizeProvider createPopupSizeProvider() {
   templateUrl: 'app_component.html',
   directives: [
     routerDirectives,
-    DefaultPopupSizeProvider,
     LoginComponent,
     JobListComponent,
     NgIf,
@@ -65,16 +54,12 @@ PopupSizeProvider createPopupSizeProvider() {
     MaterialPersistentDrawerDirective,
     MaterialListComponent,
     MaterialListItemComponent,
-    MaterialPopupComponent,
-    PopupSourceDirective,
     MaterialListComponent,
     MaterialListItemComponent,
-    FullscreenSpinnerComponent,
   ],
   pipes: [BlocPipe],
   providers: [
     AuthGuard,
-    popupBindings,
     routerProvidersHash,
     ClassProvider(AuthProvider),
     ClassProvider(JoblistProvider),
@@ -99,15 +84,16 @@ class AppComponent implements OnInit, OnDestroy {
   UserBloc userBloc;
   PdfBloc pdfBloc;
 
+  // the [User] as shown in the drawer header
+  User user;
+
+  // true if [AuthBloc] yields authorized state; visibility of navigation and headerbar depends on this
   bool authorized = false;
-  bool navOptionsVisible = false;
-  RelativePosition popupPosition = RelativePosition.OffsetBottomLeft;
-  bool customWidth = true;
-  bool appBusy = false;
-  StreamSubscription<Event> uploadListener;
+
+  // whether scanning link should be active in the drawer; defaults to false
   bool enableScanning = false;
 
-  User user;
+  StreamSubscription<Event> uploadListener;
   StreamSubscription userListener;
 
   Timer refreshTimer;
@@ -145,55 +131,44 @@ class AppComponent implements OnInit, OnDestroy {
 
   @override
   void ngOnInit() {
-    authBloc.state.listen((AuthState state) {
-      if (state.isBusy) {
-        appBusy = true;
-      } else if (state.isAuthorized) {
-        authorized = true;
-        appBusy = false;
-        joblistBloc.onStart(state.token);
-        uploadBloc.onStart(state.token);
-        previewBloc.onStart(state.token);
-        printQueueBloc.onStart(state.token);
-        pdfBloc.onStart(state.token);
-        userBloc.onStart(state.token);
-        onLogin();
-      } else if (state.isUnauthorized) {
-        authorized = false;
-        appBusy = false;
-      } else if (state.isException) {
-        appBusy = false;
-        authorized = false;
-      }
-    });
+    authBloc.state.listen(
+      (AuthState state) {
+        if (state.isAuthorized) {
+          authorized = true;
+          joblistBloc.onStart(state.token);
+          uploadBloc.onStart(state.token);
+          previewBloc.onStart(state.token);
+          printQueueBloc.onStart(state.token);
+          pdfBloc.onStart(state.token);
+          userBloc.onStart(state.token);
+          onLogin();
+        } else if (state.isUnauthorized) {
+          authorized = false;
+        } else if (state.isException) {
+          authorized = false;
+        }
+      },
+    );
 
+    // call [onLogout] if logout event from custom JS is received
     document.on["logout"].listen((Event event) {
       onLogout();
     });
 
     // check for direct printers to scan from
-    enableScanning =
-        (const String.fromEnvironment('leftPrinter', defaultValue: '')
-                .isNotEmpty ||
-            const String.fromEnvironment('rightPrinter', defaultValue: '')
-                .isNotEmpty);
-
-    userListener = userBloc.state.listen((UserState state) {
-      if (state.isResult) {
-        user = state.value;
-      }
-    });
+    enableScanning = (const String.fromEnvironment('leftPrinter', defaultValue: '').isNotEmpty ||
+        const String.fromEnvironment('rightPrinter', defaultValue: '').isNotEmpty);
   }
 
-  onLogin() {
+  void onLogin() {
     // Listen for uploadJob event to be called by our custom JS
     uploadListener = document.on["uploadJob"].listen((Event event) {
       CustomEvent ce = (event as CustomEvent);
 
       // This converts event's payload from JSON to a Dart Map.
-      Map payload = jsonDecode(ce.detail);
+      Map<String, dynamic> payload = json.decode(ce.detail);
       String filename = payload['filename'];
-      List<int> data = base64Decode(payload['data']);
+      List<int> data = base64.decode(payload['data']);
 
       uploadBloc.onUpload(data, filename: filename);
     });
@@ -201,13 +176,21 @@ class AppComponent implements OnInit, OnDestroy {
     // Tell our custom JS to start watching for fakeprinting
     document.dispatchEvent(new CustomEvent("loggedIn"));
 
-    refreshTimer = Timer.periodic(Duration(minutes: 1), (Timer t) {
+    // refresh user and joblist twice every minute to keep it synchronized
+    refreshTimer = Timer.periodic(Duration(seconds: 30), (Timer t) {
       userBloc.onRefresh();
       joblistBloc.onRefresh();
     });
+
+    // listen for new [User]s from the bloc and set local [user]
+    userListener = userBloc.state.listen((UserState state) {
+      if (state.isResult) {
+        user = state.value;
+      }
+    });
   }
 
-  onLogout() {
+  void onLogout() {
     if (authorized) {
       window.localStorage.remove('token');
       if (authBloc != null) {
@@ -226,13 +209,8 @@ class AppComponent implements OnInit, OnDestroy {
     }
   }
 
-  onOpenDialog() {
-    document.dispatchEvent(new CustomEvent("showOpenPDF"));
+  /// change state of drawer to whatever is the opposite of the state it had before
+  void onToggleDrawer() {
+
   }
 }
-
-@Directive(
-  selector: '[defaultPopupSizeProvider]',
-  providers: [Provider(PopupSizeProvider, useFactory: createPopupSizeProvider)],
-)
-class DefaultPopupSizeProvider {}
