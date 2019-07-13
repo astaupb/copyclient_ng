5165be8bddc13fdb0246d35858e2b4ad2c0b93e9
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-07-13 02:32:12 +0200

    add dashboard card for transactions and increment

--- lib/src/dashboard/dashboard_component.dart
+++ lib/src/dashboard/dashboard_component.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular_components/material_list/material_list.dart';
 import 'package:angular_components/material_list/material_list_item.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:blocs_copyclient/auth.dart';
+import 'package:blocs_copyclient/journal.dart';
 import 'package:blocs_copyclient/user.dart';
+import 'package:copyclient_ng/src/providers/journal_provider.dart';
 
 import '../auth_guard.dart';
 import '../providers/auth_provider.dart';
@@ -19,28 +23,51 @@ import '../providers/user_provider.dart';
   directives: [
     MaterialListItemComponent,
     MaterialListComponent,
+    NgFor,
   ],
 )
-class DashboardComponent extends AuthGuard implements OnActivate {
+class DashboardComponent extends AuthGuard implements OnActivate, OnDeactivate {
   AuthBloc authBloc;
   UserBloc userBloc;
+  JournalBloc journalBloc;
 
   User user;
+  List<Transaction> transactions;
 
-  DashboardComponent(AuthProvider authProvider, Router router, UserProvider userProvider)
-      : super(authProvider, router) {
+  StreamSubscription userListener;
+  StreamSubscription journalListener;
+
+  DashboardComponent(
+    AuthProvider authProvider,
+    Router router,
+    UserProvider userProvider,
+    JournalProvider journalProvider,
+  ) : super(authProvider, router) {
     authBloc = authProvider.authBloc;
     userBloc = userProvider.userBloc;
+    journalBloc = journalProvider.journalBloc;
   }
 
   @override
   void onActivate(RouterState previous, RouterState current) {
-    userBloc.onRefresh();
-
-    userBloc.state.listen((UserState state) {
+    userListener = userBloc.state.listen((UserState state) {
       if (state.isResult) {
         user = state.value;
       }
     });
+
+    journalListener = journalBloc.state.listen((JournalState state) {
+      if (state.isResult) {
+        transactions = state.value.transactions.sublist(0, 5);
+      }
+    });
+
+    userBloc.onRefresh();
+  }
+
+  @override
+  void onDeactivate(RouterState current, RouterState next) {
+    userListener.cancel();
+    journalListener.cancel();
   }
 }
