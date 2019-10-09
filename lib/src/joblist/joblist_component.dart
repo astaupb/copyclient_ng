acba5ae3fcb9aaee810cac6097953c253750c69b
Michael Koch <emkay443@mail.upb.de>
2019-10-09 17:40:15 +0200

    Reset copy mode when status code is 423, also change status code detection

--- lib/src/joblist/joblist_component.dart
+++ lib/src/joblist/joblist_component.dart
@@ -306,7 +306,7 @@ class JobListComponent extends AuthGuard implements OnActivate, OnDeactivate {
             if (uploadsTimer != null) uploadsTimer.cancel();
             uploadsTimer = Timer.periodic(
                 Duration(seconds: 1), (Timer t) => uploadBloc.onRefresh());
-          } else if (state.isException && state.error.toString().contains('423')) {
+          } else if (state.isException && (state.error as ApiException).statusCode == 423) {
             errorText = 'Der Drucker ist bereits gesperrt!';
             showError = true;
             Future.delayed(const Duration(seconds: 5))
@@ -314,6 +314,7 @@ class JobListComponent extends AuthGuard implements OnActivate, OnDeactivate {
             printerLocked = false;
             deactivate(printLockTimer);
             deactivate(uploadsTimer);
+            copyMode = false;
           } else if (!state.isLocked) {
             printerLocked = false;
             deactivate(printLockTimer);
