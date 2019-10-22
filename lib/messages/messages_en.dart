010995dce13f2d519f1e259c0e138c31d1e7d620
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-10-22 20:54:14 +0200

    add existing translation work from aufwerter_ng

---
+++ lib/messages/messages_en.dart
@@ -0,0 +1,31 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+// ignore: unnecessary_new
+final messages = new MessageLookup();
+
+// ignore: unused_element
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+// ignore: non_constant_identifier_names
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "Aktuelles Guthaben:" : MessageLookupByLibrary.simpleMessage("Current credit:"),
+    "Alle Transaktionen" : MessageLookupByLibrary.simpleMessage("Alle Transaktionen"),
+    "Aufladung" : MessageLookupByLibrary.simpleMessage("Choose Credit"),
+    "Betrag" : MessageLookupByLibrary.simpleMessage("Amount"),
+    "Das ist nicht dein Benutzer? Dann logge dich bitte links beim letzten Punkt aus." : MessageLookupByLibrary.simpleMessage("Das ist nicht dein Benutzer? Dann logge dich bitte links beim letzten Punkt aus."),
+    "PayPal öffnen" : MessageLookupByLibrary.simpleMessage("PayPal öffnen"),
+    "mit Kartennummer" : MessageLookupByLibrary.simpleMessage("with card number")
+  };
+}
--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,16 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:copyclient_ng/app_component.template.dart' as ng;
+import 'package:copyclient_ng/messages/messages_all.dart';
+import 'package:intl/intl_browser.dart';
 import 'package:logging/logging.dart';
 
 import 'main.template.dart' as self;
 
-void main() {
+void main() async {
+  final locale = await findSystemLocale();
+  await initializeMessages(locale);
+
   runApp(ng.AppComponentNgFactory);
 
   Logger.root.level = Level.ALL;
